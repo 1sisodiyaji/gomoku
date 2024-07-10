153d8703234824ac380b80059b1cad59df9dafbc
1sisodiyaji <637golusingh@gmail.com>
2024-07-11 00:17:24 +0530

    commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
-const config = {
-    // BASE_URL: 'https://server-fl9q.onrender.com'
+const config = { 
   
-         BASE_URL: 'http://localhost:8081'
-    // BASE_URL: "https://codesaarthiserver.vercel.app",
+ //   BASE_URL: 'http://localhost:8081'
+//     BASE_URL: "https://gomokuserver.vercel.app",
+     BASE_URL : "https://gomoku-server.vercel.app"
   };
   
   export default config;
