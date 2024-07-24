678fac4ce78e975ec889952f61d038883fb335c9
Golu_singh <637golusingh@gmail.com>
2024-07-24 20:30:02 +0530

    update

--- src/component/FootNavbar.js
+++ src/component/FootNavbar.js
@@ -4,7 +4,7 @@ import config from "../config/config";
 import axios from "axios";
 export const FootNabar = () => {
   const location = useLocation();
-  const [user, setUser] =useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -12,7 +12,7 @@ export const FootNabar = () => {
 
   useEffect(() => {
     const fetchUserData = async () => {
-      const token = sessionStorage.getItem('token'); 
+      const token = sessionStorage.getItem("token");
       if (token) {
         try {
           const response = await axios.post(
@@ -23,7 +23,7 @@ export const FootNabar = () => {
                 Authorization: `Bearer ${token}`,
               },
             }
-          ); 
+          );
           if (response.data.status === "success") {
             setUser(response.data.user);
           } else {
@@ -42,39 +42,59 @@ export const FootNabar = () => {
     <>
       {/* <!-- Navbar For small screen--> */}
       <nav
-        className="navbar navbar-expand-lg   fixed-bottom d-lg-none d-md-none d-sm-block  shadow-0" style={{backgroundColor: '#001926'}}
+        className="navbar navbar-expand-lg   fixed-bottom d-lg-none d-md-none d-sm-block  shadow-0"
+        style={{ backgroundColor: "#001926" }}
       >
         <div className="container-fluid d-flex justify-content-around  align-items-center p-1">
-          <Link className="text-center" style={{ fontSize: "1.4rem" }} to="/"> 
-            <i className="fi fi-rr-house-window iconColor"></i>{" "}
-          </Link>
-          <Link  className="text-center"  style={{ fontSize: "1.4rem" }}to="/Rules"  > 
+          {user ? (
+            <Link
+              className="text-center"
+              style={{ fontSize: "1.4rem" }}
+              to="/dashboard"
+            >
+              <i className="fi fi-rr-house-window iconColor"></i>{" "}
+            </Link>
+          ) : (
+            <Link className="text-center" style={{ fontSize: "1.4rem" }} to="/">
+              <i className="fi fi-rr-house-window iconColor"></i>{" "}
+            </Link>
+          )}
+          <Link
+            className="text-center"
+            style={{ fontSize: "1.4rem" }}
+            to="/Rules"
+          >
             <i className="fi fi-rr-users-alt iconColor"></i>
           </Link>
-           
+
           <Link
             className="text-center"
             style={{ fontSize: "1.4rem" }}
             to="/Your-Rewards"
           >
-           <i className="fi fi-sr-trophy-star iconColor "></i>
+            <i className="fi fi-sr-trophy-star iconColor "></i>
           </Link>
           <Link
             className="text-center"
             style={{ fontSize: "1.4rem" }}
             to="/profile"
           >
-              {user && user.image ? 
-              <>  <img
-              src={user.image}
-              className="rounded-circle border"
-              height="28"
-              alt="."
-              loading="lazy"
-            /></>
-            :
-            <><i className="fi fi-rr-circle-user iconColor"></i></>}
-          
+            {user && user.image ? (
+              <>
+                {" "}
+                <img
+                  src={user.image}
+                  className="rounded-circle border"
+                  height="28"
+                  alt="."
+                  loading="lazy"
+                />
+              </>
+            ) : (
+              <>
+                <i className="fi fi-rr-circle-user iconColor"></i>
+              </>
+            )}
           </Link>
         </div>
       </nav>
